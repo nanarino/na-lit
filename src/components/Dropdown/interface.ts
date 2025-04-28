2d2ab5c0d1b9bf91407fafe7dd6181d7e334ab44
☘ <kogawananari@gmail.com>
2025-04-28 16:08:03 +0800

    :pencil:

--- src/components/Dropdown/interface.ts
+++ src/components/Dropdown/interface.ts
@@ -1,10 +1,12 @@
+type cssText = string
+
 export interface DropdownProps extends Omit<Partial<HTMLElement>, "children"> {
     /** 氣泡框彈出模式 */
     "dialog-popover"?: "auto" | "manual" | "hint"
     /** 氣泡框彈出層樣式 */
-    "dialog-style"?: string
+    "dialog-style"?: cssText
     /** 自動觸發關閉的選擇器 */
     closetarget?: string
-    /** 立即關閉 */
+    /** 立即關閉 lit 的 boolean 實際是 `'true' | 'false'` 啦 */
     closesoon?: boolean
 }
